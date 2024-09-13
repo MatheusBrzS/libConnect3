34618e72b5c0be23ed860d25079038eaa5fb36e2
unknown <soares.lopes@mail.uft.edu.br>
2024-09-13 16:35:56 -0300

    add default page

--- main/urls.py
+++ main/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path, include
+from django.views.generic import TemplateView
 from main.views import (
     IndexTemplateView,
     LivroDetailView,
@@ -9,4 +10,5 @@ app_name = 'main'
 urlpatterns = [
     path('', IndexTemplateView.as_view(), name='index'),
     path('livro/<int:pk>/', LivroDetailView.as_view(), name='livro-detail'),
+    path('default/', TemplateView.as_view(template_name="default.html"), name='default'),
 ]
