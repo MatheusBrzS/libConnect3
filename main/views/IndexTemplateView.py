9980930b90ee4bc0f384e52518de5004297a0273
Rafael Soares <79319841+RafaelSoares12@users.noreply.github.com>
2024-11-19 14:43:21 -0300

    add filters

--- main/views/IndexTemplateView.py
+++ main/views/IndexTemplateView.py
@@ -2,6 +2,7 @@ from typing import Any
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
+from main.models import Categoria
 from main.models.Livro import Livro
 
 class IndexTemplateView(LoginRequiredMixin, TemplateView):
@@ -9,5 +10,17 @@ class IndexTemplateView(LoginRequiredMixin, TemplateView):
 
     def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
         context = super().get_context_data(**kwargs)
-        context['livros'] = Livro.objects.all()
+        
+        livros = Livro.objects.all()
+
+        categoriaId = self.request.GET.get('categoria')
+        if categoriaId:
+            livros = livros.filter(categoria__id=categoriaId)
+
+        autor = self.request.GET.get('autor')
+        if autor:
+            livros = livros.filter(autor__icontains=autor)
+
+        context['livros'] = livros
+        context['categorias'] = Categoria.objects.all() 
         return context
